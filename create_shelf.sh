c68fba1891f7e23d3bf26a9958c0d61f05a8182b
Greg Farris <farrisg@umich.edu>
2019-01-18 16:04:11 -0500

    updated create shelf script to handle released and pre released kos

--- create_shelf.sh
+++ create_shelf.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+source config.conf
 shelfUrl=$1
-
+repos=($2)
 if [[ -n "shelfUrl" ]]; then
   rm -rf build
   mkdir build
   cd build
 
-  repos=(opioid-collection cpic-collection example-collection cancer-risk-collection icon-array script-numerate postpci labwise )
-
   for i in "${repos[@]}"
   do
-     url=(https://api.github.com/repos/kgrid-objects/$i/releases/latest  )
+     echo -e "Download release  $i "
+     if [[ $i == *:* ]]; then
+       theReop=$(echo $i | cut -d':' -f 1)
+       theTag=$(echo $i | cut -d':' -f 2)
+       url=(https://api.github.com/repos/kgrid-objects/$theReop/releases/tags/$theTag?access_token=$GIT_TOKEN )
+     else
+       url=(https://api.github.com/repos/kgrid-objects/$i/releases/latest?access_token=$GIT_TOKEN  )
+     fi
      ../download.sh "$url"
   done
 
-  for zipFile in *.koio.zip
+  for zipFile in *.zip
   do
     echo -e "Processing $zipFile"
     fileName=${zipFile%%.*}
--- download.sh
+++ download.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo Assests from repo $1
 
 artifacts=($(curl -s $1 | jq -r ".assets[].browser_download_url"))
-
+echo ${#artifacts[@]} assests downloading
 for url in "${artifacts[@]}"
 do
 	curl -s -L -O $url
