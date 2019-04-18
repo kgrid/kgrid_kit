3d4dcf9d9fd96bc9a1b6b975526a6e4dce1e71dd
gregfarris <farrisg@umich.edu>
2019-04-18 11:28:23 -0400

    updated shelf instance scripts

--- lion/shelfupdate.sh
+++ lion/shelfupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../create_shelf.sh \
+(cd ..; .//create_shelf.sh \
 "http://library.kgrid.org/shelf http://activator.kgrid.org" \
 "score opioid-collection cpic-collection example-collection cancer-risk-collection \
-icon-array script-numerate postpci labwise ipp-collection"
+icon-array script-numerate postpci labwise ipp-collection")
--- monkey/shelfupdate.sh
+++ monkey/shelfupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../create_shelf.sh \
+(cd ..; .//create_shelf.sh \
 "http://monkey-library.kgrid.org/shelf http://monkey-activator.kgrid.org" \
 "score opioid-collection cpic-collection example-collection cancer-risk-collection \
-icon-array script-numerate postpci labwise ipp-collection"
+icon-array script-numerate postpci labwise ipp-collection")
