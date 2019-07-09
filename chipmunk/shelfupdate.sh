4ac1581c215349360ac2dc94789b4b826976636b
gregfarris <farrisg@umich.edu>
2019-07-09 17:41:35 -0400

    [#167182055] updated for release

--- chipmunk/shelfupdate.sh
+++ chipmunk/shelfupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 (cd ..; .//create_shelf.sh \
-"https://kgrid-library.herokuapp.com/shelf https://kgrid-activator.herokuapp.com/kos" \
+"https://kgrid-library.herokuapp.com/shelf/manifest https://kgrid-activator.herokuapp.com/kos/manifest" \
 "score opioid-collection cpic-collection example-collection cancer-risk-collection \
 icon-array script-numerate postpci labwise ipp-collection")
--- lion/shelfupdate.sh
+++ lion/shelfupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 (cd ..; .//create_shelf.sh \
-"http://library.kgrid.org/shelf http://activator.kgrid.org/kos/manifest" \
+"http://library.kgrid.org/shelf/manifest http://activator.kgrid.org/kos/manifest" \
 "score opioid-collection cpic-collection example-collection cancer-risk-collection \
 icon-array script-numerate postpci labwise ipp-collection")
