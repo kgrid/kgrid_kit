007dd7478b18689047cc889267cbaafdf7792d1a
gregfarris <farrisg@umich.edu>
2019-07-09 17:39:14 -0400

    [#167182055] updated for release

--- hippo/shelfupdate.sh
+++ hippo/shelfupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 (cd ..; ./create_shelf.sh \
-"http://hippo-library.kgrid.org/shelf http://hippo-activator.kgrid.org/kos" \
+"http://hippo-library.kgrid.org/shelf http://hippo-activator.kgrid.org/kos/manifest" \
 "score opioid-collection cpic-collection example-collection cancer-risk-collection \
 icon-array script-numerate postpci labwise ipp-collection")
